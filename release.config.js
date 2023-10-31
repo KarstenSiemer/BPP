e5a41d54eb3497a1aa7e7e050d5e822b600010a5
Karsten Siemer <karsten.siemer@sda.se>
2023-10-31 16:01:21 +0100

    feat(workflows): enhance PR and release workflows ℹ️ This update is all about refining the workflow structure. We've got it down to a science! 🧪🔬
    Changes include:
    - Streamlined event triggers for PR workflow
    - Added Semantic release step 📦 in PR workflow
    - Configured extra plugins for Semantic release in PR and release workflows
    - Added update push step 🔝 in release workflow for major version modifications
    - Added new `release.config.js` file 🆕 to define release configurations.
    
    I guess you can say, we're releasing the potential of our workflows! 😉
    
    Signed-off-by: Karsten Siemer <karsten.siemer@sda.se>

---
+++ release.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "dryRun": false,
+    "plugins": [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/changelog",
+            {
+                "changelogFile": "docs/CHANGELOG.md"
+            }
+        ],
+        "@semantic-release/npm",
+        "@semantic-release/github",
+        [
+            "@semantic-release/git",
+            {
+                "assets": [
+                    "docs/CHANGELOG.md",
+                    "thesis.pdf"
+                ],
+                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+            }
+        ]
+    ]
+};
