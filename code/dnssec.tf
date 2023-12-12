acd0dbf732e5f97e53fcde13bf6cf2449d301d33
Karsten Siemer <karsten.siemer@sda.se>
2023-12-12 08:25:40 +0100

    feat(dnssec): enable DNSSEC in AWS using Terraform ✨ Add Terraform configuration for enabling DNSSEC in AWS ℹ️ This commit provides a Terraform script to enable Domain Name System Security Extensions (DNSSEC) in AWS using the Key Management Service (KMS). DNSSEC is a suite of Internet Engineering Task Force (IETF) specifications for securing certain kinds of information provided by the DNS. This allows clients to check the integrity and authenticity of the responses they get. The script includes the following modifications: - It creates and configures a KMS key for DNSSEC. - It sets up DNSSEC for several hosted zones. - It creates Key Signing Keys (KSK) for the hosted zones. - Ensures secure connection to Route 53 DNS Service.
    In addition, the script appears to also manage DNS records for several sub-domains.
    
    Also, DNSSEC-related content is added in einleitung.tex (Introduction). The example configuration has been removed
    
    Signed-off-by: Karsten Siemer <karsten.siemer@sda.se>

---
+++ code/dnssec.tf
@@ -0,0 +1,138 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "dnssec_key" {
+  customer_master_key_spec = "ECC_NIST_P256"
+  deletion_window_in_days  = 7
+  key_usage                = "SIGN_VERIFY"
+  policy = jsonencode({
+    Statement = [
+      {
+        Action = [
+          "kms:DescribeKey",
+          "kms:GetPublicKey",
+          "kms:Sign",
+        ],
+        Effect = "Allow",
+        Principal = {
+          Service = "dnssec-route53.amazonaws.com"
+        },
+        Sid      = "Allow Route 53 DNSSEC Service",
+        Resource = "*",
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = data.aws_caller_identity.current.account_id
+          },
+          ArnLike = {
+            "aws:SourceArn" = "arn:aws:route53:::hostedzone/*"
+          }
+        }
+      },
+      {
+        Action = "kms:CreateGrant",
+        Effect = "Allow",
+        Principal = {
+          Service = "dnssec-route53.amazonaws.com"
+        },
+        Sid      = "Allow Route 53 DNSSEC Service to CreateGrant",
+        Resource = "*",
+        Condition = {
+          Bool = {
+            "kms:GrantIsForAWSResource" = "true"
+          }
+        }
+      },
+      {
+        Action = "kms:*",
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Resource = "*",
+        Sid      = "Enable IAM User Permissions"
+      },
+    ],
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_route53_zone" "sda_se_io_zone" {
+  name = "sda-se.io"
+}
+
+resource "aws_route53_zone" "projekt1_public_zone" {
+  name = "projekt1.sda-se.io"
+}
+
+resource "aws_route53_zone" "dev_projekt1_public_zone" {
+  name = "dev.projekt1.sda-se.io"
+}
+
+resource "aws_route53_record" "sda_se_io_to_projekt1" {
+  zone_id = aws_route53_zone.sda_se_io_zone.zone_id
+  name    = "projekt1"
+  type    = "NS"
+  ttl     = 300
+  records = aws_route53_zone.projekt1_public_zone.name_servers
+}
+
+resource "aws_route53_record" "projekt1_to_dev_projekt1_public" {
+  zone_id = aws_route53_zone.projekt1_public_zone.zone_id
+  name    = "dev"
+  type    = "NS"
+  ttl     = 300
+  records = aws_route53_zone.dev_projekt1_public_zone.name_servers
+}
+
+resource "aws_route53_key_signing_key" "dev_projekt1_public_zone" {
+  hosted_zone_id             = aws_route53_zone.dev_projekt1_public_zone.id
+  key_management_service_arn = aws_kms_key.dnssec_key.arn
+  name                       = "dev"
+}
+
+resource "aws_route53_hosted_zone_dnssec" "dev_projekt1_public_zone" {
+  hosted_zone_id = aws_route53_zone.dev_projekt1_public_zone.id
+}
+
+resource "aws_route53_key_signing_key" "projekt1_public_zone" {
+  hosted_zone_id             = aws_route53_zone.projekt1_public_zone.id
+  key_management_service_arn = aws_kms_key.dnssec_key.arn
+  name                       = "projekt1"
+}
+
+resource "aws_route53_hosted_zone_dnssec" "projekt1_public_zone" {
+  hosted_zone_id = aws_route53_zone.projekt1_public_zone.id
+}
+
+resource "aws_route53_record" "projekt1_to_dev_projekt1_public" {
+  zone_id = aws_route53_zone.projekt1_public_zone.zone_id
+  name    = "dev"
+  type    = "DS"
+  ttl     = 300
+  records = [
+    aws_route53_key_signing_key.dev_projekt1_public_zone.ds_record,
+  ]
+}
+
+resource "aws_route53_key_signing_key" "projekt1_public_zone" {
+  hosted_zone_id             = aws_route53_zone.sda_se_io_zone.id
+  key_management_service_arn = aws_kms_key.dnssec_key.arn
+  name                       = "sda-se"
+}
+
+resource "aws_route53_hosted_zone_dnssec" "projekt1_public_zone" {
+  hosted_zone_id = aws_route53_zone.sda_se_io_zone.id
+}
+
+resource "aws_route53_record" "projekt1_to_dev_projekt1_public" {
+  zone_id = aws_route53_zone.sda_se_io_zone.zone_id
+  name    = "projekt1"
+  type    = "DS"
+  ttl     = 300
+  records = [
+    aws_route53_key_signing_key.projekt1_public_zone.ds_record,
+  ]
+}
